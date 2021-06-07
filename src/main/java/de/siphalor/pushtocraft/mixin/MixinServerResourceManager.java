0f0ed41e480dac90234f80bffb2821edff751185
Siphalor <info@siphalor.de>
2021-06-08 00:10:03 +0200

    Update to 1.17

--- src/main/java/de/siphalor/pushtocraft/mixin/MixinServerResourceManager.java
+++ src/main/java/de/siphalor/pushtocraft/mixin/MixinServerResourceManager.java
@@ -3,7 +3,7 @@ package de.siphalor.pushtocraft.mixin;
 import de.siphalor.pushtocraft.PushToCraftManager;
 import net.minecraft.recipe.RecipeManager;
 import net.minecraft.resource.ReloadableResourceManager;
-import net.minecraft.resource.ResourceReloadListener;
+import net.minecraft.resource.ResourceReloader;
 import net.minecraft.resource.ServerResourceManager;
 import net.minecraft.server.MinecraftServer;
 import org.spongepowered.asm.mixin.Final;
@@ -23,24 +23,13 @@ public class MixinServerResourceManager {
 
 	@Inject(method = "<init>", at = @At("RETURN"))
 	public void onConstructed(CallbackInfo callbackInfo) {
-		List<ResourceReloadListener> listeners = ((ReloadableResourceManagerImplAccessor) resourceManager).getListeners();
-		List<ResourceReloadListener> initialListeners = ((ReloadableResourceManagerImplAccessor) resourceManager).getInitialListeners();
+		List<ResourceReloader> listeners = ((ReloadableResourceManagerImplAccessor) resourceManager).getReloaders();
 		PushToCraftManager pushToCraftManager = new PushToCraftManager();
-		boolean awaitL = true, awaitIL = true;
-		for (int i = 0, l = Math.min(listeners.size(), initialListeners.size()); i < l; i++) {
-			if (awaitL && listeners.get(i) == recipeManager) {
+
+		for (int i = 0; i < listeners.size(); i++) {
+			if (listeners.get(i) == recipeManager) {
 				listeners.add(i, pushToCraftManager);
-				awaitL = false;
-				if (!awaitIL) {
-					break;
-				}
-			}
-			if (awaitIL && initialListeners.get(i) == recipeManager) {
-				initialListeners.add(i, pushToCraftManager);
-				awaitIL = false;
-				if (!awaitL) {
-					break;
-				}
+				return;
 			}
 		}
 	}
--- src/main/java/de/siphalor/pushtocraft/mixin/ReloadableResourceManagerImplAccessor.java
+++ src/main/java/de/siphalor/pushtocraft/mixin/ReloadableResourceManagerImplAccessor.java
@@ -1,7 +1,7 @@
 package de.siphalor.pushtocraft.mixin;
 
 import net.minecraft.resource.ReloadableResourceManagerImpl;
-import net.minecraft.resource.ResourceReloadListener;
+import net.minecraft.resource.ResourceReloader;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
@@ -10,7 +10,5 @@ import java.util.List;
 @Mixin(ReloadableResourceManagerImpl.class)
 public interface ReloadableResourceManagerImplAccessor {
 	@Accessor
-	List<ResourceReloadListener> getListeners();
-	@Accessor
-	List<ResourceReloadListener> getInitialListeners();
+	List<ResourceReloader> getReloaders();
 }
